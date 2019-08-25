c4b7ff992243f3bf6085a56f8ef9ca4ef0bd7236
Administrator <gabrielsule@gmail.com>
2019-08-25 09:44:15 -0300

    first commit

---
+++ RedisApp/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using ServiceStack.Redis;
+
+namespace RedisApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var manager = new RedisManagerPool("localhost:6379");
+            using (var client = manager.GetClient())
+            {
+                client.Set("foo", "bar");
+                Console.WriteLine("foo={0}", client.Get<string>("foo"));
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
